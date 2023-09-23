0201f2a2cc58d290df2d9b92fb1e4391c1eddf07
ahmed1 <34408378+ahmed1@users.noreply.github.com>
2023-09-23 18:56:06 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Neuranest",
-            url: "https://github.com/ahmed1/test-framework/releases/download/v0.0.1/Neuranest.zip",
-            checksum: "84fa305ddb223e9be79c486ad28a31821528f0944e8e76aaf654f8c3828263d6"
+            url: "https://github.com/ahmed1/test-framework/releases/download/v0.0.2/Neuranest.zip",
+            checksum: "68272ee07129a57efe4dcf401ff2991382b44f7021e48d87d5ec67c1978510da"
         )
     ]
 )
